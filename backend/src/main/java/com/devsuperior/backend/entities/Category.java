061ffb84a16ed2599daad6641c96c098678eae1b
Junior Daliessi <juniordaliessi@gmail.com>
2023-04-16 20:38:47 -0300

    created the Category class

---
+++ backend/src/main/java/com/devsuperior/backend/entities/Category.java
@@ -0,0 +1,64 @@
+package com.devsuperior.backend.entities;
+
+import java.io.Serializable;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    //Atributos
+    private Long id;
+    private String name;
+
+    //Construtores
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //HashCode e Equals
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Category other = (Category) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
+}
