2856e905020e31151e6c220b277b60eebe9564f0
Junior Daliessi <juniordaliessi@gmail.com>
2023-04-19 03:21:15 -0300

    Transactions and JPA session

--- backend/src/main/java/com/devsuperior/backend/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/backend/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.backend.entities.Category;
 import com.devsuperior.backend.repositories.CategoryRepository;
@@ -16,6 +17,7 @@ public class CategoryService {
     @Autowired //Anotação para indicar que a dependência vai ser injetada automaticamente pelo Spring
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true) //Anotação para indicar que o método é transacional e que a operação é somente leitura
     public List<Category> findAll() {
         return repository.findAll();
     }
